9626d4eeb30b8f59bd1f6f0de3dad96aaebf54d8
jpryor812 <jpryor812@gmail.com>
2025-02-24 19:53:20 -0500

    fixed blog order

--- const/blogPosts.ts
+++ const/blogPosts.ts
@@ -1,5 +1,18 @@
 export const blogPosts = [
-    {
+  {
+    id: 2,
+    image: {
+      src: "/payment-blog-header.png",
+      alt: "Payments",
+      width: 400,
+      height: 300
+    },
+    date: "February 24, 2024",
+    title: 'Why Transaction Fees Are Quietly Killing Your Earnings as a Creator',
+    description:'Creators charging $5 or less for their content are lsing 10%+ in payment processing fees. With Sumu, that number goes to 0% no matter how much you charge for your content. In this blog post...',
+    link: "/blog/posts/payment-processing-fees" 
+  },  
+  {
       id: 1,
       image: {
         src: "/asianometry-header.png",
@@ -11,18 +24,5 @@ export const blogPosts = [
       title: "Patreon vs. Sumu Fees for a Large YouTube Channel: A Case Study",
       description: "How you could retain 95-105% of your premium content earnings on Sumu instead of 85% with Patreon? We explain with an example using the popular YouTube channel Asianometry...",
       link: "/blog/posts/sumu-fees-vs-patreon" 
-    },
-    {
-      id: 2,
-      image: {
-        src: "/payment-blog-header.png",
-        alt: "Payments",
-        width: 400,
-        height: 300
-      },
-      date: "February 24, 2024",
-      title: 'Why Transaction Fees Are Quietly Killing Your Earnings as a Creator',
-      description:'Creators charging $5 or less for their content are lsing 10%+ in payment processing fees. With Sumu, that number goes to 0% no matter how much you charge for your content. In this blog post...',
-      link: "/blog/posts/payment-processing-fees" 
     },
   ];
